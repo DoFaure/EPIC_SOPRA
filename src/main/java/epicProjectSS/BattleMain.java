c02d35525112c3c77c5c30e4c21af8c861525bce
Zykher <dorianfaure69@gmail.com>
2019-11-13 11:20:37 +0100

    Initialisation

---
+++ src/main/java/epicProjectSS/BattleMain.java
@@ -0,0 +1,46 @@
+package epicProjectSS;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import javax.ws.rs.GET;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+public class BattleMain {
+	public static void main(String[] args) throws ParseException {
+		
+		Options options = new Options();	
+		
+		options.addOption("p", "pong", false, "Print pong");
+		options.addOption("config", "conf", false, "Print something");
+		
+		 CommandLineParser parser = new DefaultParser();
+	     CommandLine cmd = parser.parse( options, args);
+
+		try {
+				if(cmd.hasOption("p")) {
+					System.out.println("pong");
+				}
+				if(cmd.hasOption("config")) {
+					Properties prop = new Properties();
+					FileInputStream input = new FileInputStream("C:\\Users\\doria\\eclipse-workspace\\epicProjectSS\\src\\main\\resources\\configuration.properties");
+					prop.load(input);
+					System.out.println(prop);
+				}
+			
+		}catch(Exception e) {
+			  //  Block of code to handle errors
+		}
+		
+	
+		
+	}
+	
+
+}
+
---
+++ src/main/java/epicProjectSS/RestAPI.java
@@ -0,0 +1,70 @@
+package epicProjectSS;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+public class RestAPI {
+
+	/* Test Appel API */
+	public void testPong() {
+		
+	}
+	
+	/* Utilisateurs */
+	
+	public void idEquipe(String nomEquipe, String motDePasse) {
+		
+	}
+	
+	/* Initialisation affrontement entre joueurs */
+	
+	public void idPartie(Integer idEquipe) {
+		
+	}
+	
+	
+	/* Initialisation affrontement contre les bots */
+	
+	public void nouvellePartie(Integer numberBot) {
+		
+	}
+	
+	/* Déroulement de la partie */
+	
+	public void tourEquipe(Integer idPartie, Integer idEquipe) {
+		
+	}
+	
+	public void plateauJeu(Integer idPartie) {
+		
+	}
+	
+	public void plateauJeuEquipe(Integer idPartie, Integer idEquipe) {
+		
+	}
+	
+	
+	public void dernierCoupJouer(Integer idPartie, Integer idEquipe) {
+		
+	}
+	
+	public void donnerCoup(Integer idPartie, Integer idEquipe, String move) {
+		
+	}
+	
+	public void nomAdversaire(Integer idPartie, Integer idEquipe) {
+		
+	}
+	
+	
+	
+	
+
+}
